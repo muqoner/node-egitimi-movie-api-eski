2df4d42f9fc195579c32d38ce3221c58a61173d8
unknown <easlanci@gmail.com>
2020-05-28 22:41:09 +0300

    /movies/between_endpoint

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require("mongoose");
-// const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 
 var indexRouter = require('./routes/index');
 var movie = require('./routes/movie');
@@ -34,7 +34,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/api/movie', movie);
+app.use('/api/movies', movie);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -49,7 +49,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({ error: { message: err.message, code: err.code}});
 });
 
 module.exports = app;
--- routes/movie.js
+++ routes/movie.js
@@ -2,6 +2,71 @@ var express = require('express');
 var router = express.Router();
 const Movie = require("../models/Movie_Schema");
 
+// api/movies anasayfaya get atma
+router.get("/",(req,res)=>{
+  const promise = Movie.find({ });
+  promise.then((data)=>{
+    res.json(data)
+  }).catch((err)=>{
+    res.json(err);
+  })
+});
+//top10 listeleme
+router.get("/top10",(req,res)=>{
+  const response = Movie.find({ }).limit(10).sort({"imdb_score" : -1});
+  response.then((data)=>{
+    res.json(data)
+  }).catch((err)=>{
+    res.json(err)
+  })
+});
+
+//Between 
+router.get("/between/:start_year/:end_year",(req,res)=>{
+  const {start_year, end_year} = req.params;
+  const promise = Movie.find({year:{"$gte":parseInt(start_year), 
+    "$lte":parseInt(end_year)}});
+  promise.then((data)=>{
+    res.json(data)
+  }).catch((err)=>{
+    res.json(err)
+  })
+})
+
+// Id bazlı sorgu yapma
+
+router.get("/:movie_id",(req,res,next)=>{
+  const promise = Movie.findById(req.params.movie_id);
+  promise.then((movie)=>{
+    if(!movie)
+      next({message: "aradığınız id ile eşleşen film bulunamadı", code: 99});
+    
+    res.json(movie)
+  }).catch((err)=>{
+    res.json(err.message);
+  })
+});
+
+//id ile put işlemi
+router.put("/:movie_id",(req,res)=>{
+  const promise = Movie.findByIdAndUpdate(req.params.movie_id,req.body,{new: true}); // new:true---> donecek data artık eskisi değil değişikliğe uğramış guncel data
+  promise.then((data)=>{
+    res.json(data);
+  }).catch((err)=>{
+    res.json(err);
+  })
+});
+
+// delete endpoint
+
+router.delete("/:movie_id",(req,res)=>{
+  const promise = Movie.findByIdAndRemove(req.params.movie_id);
+  promise.then(res.json("veri başarıyla silindi"))
+  .catch((err)=>{
+    res.json(err);
+  })
+})
+
 
 router.post('/', function(req, res, next) {
   // const {title, imdb_score, category, country, year} = req.body;
